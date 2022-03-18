824048c669fd10d5c80ceeccdef8d4d4f2543dcc
willbraken <brandon@willbraken.com>
2022-03-17 21:24:24 -0500

    create working game

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 
 let computerResponse = getRandomInt(1, 3); /* assign a number 1-3 for the computer's choice*/
 let computerResult = computerPlay();
+let humanInput = prompt('Rock, Paper, or Scissors?');
+let humanResult = humanInput.toLowerCase();
+
+
+
 
 function computerPlay() {
     if (computerResponse == 1) {
@@ -16,3 +21,23 @@ function computerPlay() {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); /* returns numbers 1, 2, or 3 randomly */
 }
+
+function game() {
+    if (computerResult == 'rock' && humanResult == 'paper') {
+        return 'You Win!!!';
+    } else if (computerResult == 'rock' && humanResult == 'scissors') {
+        return 'You Lose! Try Again.';
+    } else if (computerResult == 'paper' && humanResult == 'scissors') {
+        return 'You Win!!!';
+    } else if (computerResult == 'paper' && humanResult == 'rock') {
+        return 'You Lose! Try Again.';
+    } else if (computerResult == 'scissors' && humanResult == 'rock') {
+        return 'You Win!!!';
+    } else if (computerResult == 'scissors' && humanResult == 'paper') {
+        return 'You Lose! Try Again.';
+    } else {
+        return "It's a tie!"
+    }
+}
+
+console.log(game());
