14312dd41045b9c5d7150c33d779473146b1e8f8
willbraken <brandon@willbraken.com>
2022-04-25 20:23:30 -0500

    add button functionality, remove rounds

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 let compWin = 0;
 let humanWin = 0;
 
+const choice = document.querySelectorAll('button');
+
+choice.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, computerPlay());
+    });
+}); 
+
 function computerPlay(computerResponse) {
     computerResponse = getRandomInt(1, 3);
     if (computerResponse == 1) {
@@ -18,7 +26,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); /* returns numbers 1, 2, or 3 randomly */
 }
 
-
+/*
 function game() {
     for (let i = 0; i < 5; i++) {
         playRound();
@@ -34,10 +42,13 @@ function game() {
 }
 
 game();
+*/
+
+
 
 function playRound(humanResult, ComputerResult) {
-    let humanInput = prompt('Rock, Paper, or Scissors?');
-    humanResult = humanInput.toLowerCase();
+    //let humanInput = prompt('Rock, Paper, or Scissors?');
+    //humanResult = humanInput.toLowerCase();
     computerResult = computerPlay();
     if ((computerResult == 'rock' && humanResult == 'paper') || (computerResult == 'paper' && humanResult == 'scissors') || (computerResult == 'scissors' && humanResult == 'rock')) {
         console.log('You Win!!!');
