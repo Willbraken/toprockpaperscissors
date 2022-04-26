5f46a1043dfe2151bf899acd95b156d0c8f43cfd
willbraken <brandon@willbraken.com>
2022-04-25 21:32:44 -0500

    add UI & return some functionality

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 
 let compWin = 0;
 let humanWin = 0;
+let numGames = 0;
 
 const choice = document.querySelectorAll('button');
+const container = document.querySelector('#container');
+
+const gameResult = document.createElement('p');
+gameResult.classList.add('gameResult');
+
+const gameText = document.createElement('p');
+gameText.classList.add('gameText');
+
+const overallWin = document.createElement('p');
+overallWin.classList.add('overallWin');
 
 choice.forEach((button) => {
     button.addEventListener('click', () => {
         playRound(button.id, computerPlay());
+        ++numGames;
+        game();
     });
 }); 
 
@@ -25,38 +38,33 @@ function computerPlay(computerResponse) {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); /* returns numbers 1, 2, or 3 randomly */
 }
-
-/*
 function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log('Game ' + (i + 1) + '/5');
-     }
-    if (humanWin > compWin) {
-        console.log('You win overall!');
-    } else if (compWin > humanWin) {
-        console.log('You lose overall!');
-    } else {
-        console.log('Overall tie.');
-    }
+    if(humanWin > compWin && numGames === 5) {
+        overallWin.textContent = 'You Win Overall!';
+        container.appendChild(overallWin);
+    } else if(compWin > humanWin && numGames === 5) {
+        overallWin.textContent = 'You Lose Overall';
+        container.appendChild(overallWin);
+    } else if(compWin === humanWin && numGames ===5) {
+        overallWin.textContent = 'Overall Tie';
+        container.appendChild(overallWin);
+    } else;
+    gameText.textContent = 'Game ' + numGames + "/5";
+    container.appendChild(gameText);
 }
 
-game();
-*/
-
-
-
-function playRound(humanResult, ComputerResult) {
-    //let humanInput = prompt('Rock, Paper, or Scissors?');
-    //humanResult = humanInput.toLowerCase();
+function playRound(humanResult, computerResult) {
     computerResult = computerPlay();
     if ((computerResult == 'rock' && humanResult == 'paper') || (computerResult == 'paper' && humanResult == 'scissors') || (computerResult == 'scissors' && humanResult == 'rock')) {
-        console.log('You Win!!!');
+        gameResult.textContent = 'You Win!!!';
+        container.appendChild(gameResult);
         return ++humanWin;
     } else if ((computerResult == 'paper' && humanResult == 'rock') || (computerResult == 'scissors' && humanResult == 'paper') || (computerResult == 'rock' && humanResult == 'scissors')) {
-        console.log('You Lose! Try Again.');
+        gameResult.textContent = 'You Lose! Try Again.';
+        container.appendChild(gameResult);
         return ++compWin;
     } else {
-        console.log("It's a tie!");
+        gameResult.textContent = "It's a tie!";
+        container.appendChild(gameResult);
     }
     }
